c05883f7668150ecd6738e6307dc2c4732cce364
Jacob Motes <jmotes2583@gmail.com>
2021-05-24 13:27:28 -0500

    addJoke function added with post method

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,42 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+
+    //click listeners
+    
+    //create a click listener that will add user input with the 'Add Joke' button
+    $('#addJokeButton').on('click', addJoke)
+
+
+
+
+
+
+
+}
+
+//create a global object that will include the person, the question, and the punchline
+const theJoker = {
+
+};
+
+//create a function that will add a joke when the user clicks the 'Add Joke' button
+function addJoke () {
+    //testing the click listener
+    console.log('Jokes on Jokes');
+    //need to take in user input
+    theJoker.whoseJoke = $('#whoseJokeIn').val(),
+    theJoker.jokeQuestion = $('#questionIn').val(),
+    theJoker.punchLine = $('#punchlineIn').val(),
+    //test that it took in the input
+    console.log(theJoker);
+
+    //need to POST the user input to the server
+    $.ajax({
+        url: '/jokes',
+        method: 'POST',
+        data: theJoker
+    }).then(response => {
+        console.log(response);
+    })
 }
